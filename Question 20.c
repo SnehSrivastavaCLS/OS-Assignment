cab434474e5b470cb10014558a09b03f89873f28
snehsriv <snehsri99@gmail.com>
2020-04-10 00:54:59 +0530

    Updated Question 20.c file

--- Question 20.c
+++ Question 20.c
@@ -1,1 +1,50 @@
-//Question 20
+	//Question 20
+	#include<stdbool.h>
+	#include<stdio.h>
+	struct requirement
+	{
+	 	bool pen ;
+		bool paper ;
+		bool question_paper ;
+		bool all_three ;
+	};
+	int main(void)
+	{
+		int n=3;
+		struct requirement  s[n];
+		s[0].pen=true;		
+		s[0].paper = false;
+		s[0].question_paper = false;
+		s[0].all_three= false;
+		s[1].pen=false;		
+		s[1].paper = true;
+		s[1].question_paper = false;
+		s[1].all_three = false;
+		s[2].pen=false;		
+		s[2].paper = false;
+		s[2].question_paper = true;
+		s[2].all_three = false	;
+		while(s[0].all_three==false||s[1].all_three==false||s[2].all_three==false)
+		{
+			int ch1,ch2;
+			printf("\nResources:\n1.Pen\n2.Paper\n3.Question paper\n Enter the two things that are to be placed on the shared table: ");
+			scanf("%d%d",&ch1,&ch2);
+			if(ch1==1 && ch2==2 && s[2].all_three==false)
+			{
+				s[2].all_three=true ;
+				printf("Third Student has completed the task\n");
+			}
+			if(ch1==2 && ch2==3 && s[0].all_three==false)
+			{
+				s[0].all_three=true;
+				printf("First Student has completed the task\n");
+			}
+			if(ch1==1 && ch2==3 && s[1].all_three==false)
+			{
+				s[1].all_three=true;
+				printf("Second Student has completed the task\n");
+			}
+		}
+		printf("All the students now have completed their respective tasks succesfully\n");
+		return 0;
+	}
